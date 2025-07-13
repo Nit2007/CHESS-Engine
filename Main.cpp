78a97c5b24495d2b09a2361e3712053bcc89bb5e
Nit2007 <nithishjaisarun.a@gmail.com>
2025-07-13 22:12:30 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,56 @@
+#include "defs.h"
+#include "struct.h"
+
+extern int sq64tosq120[64];
+extern int sq120tosq64[BOARD_SQ_NUM];
+extern int square120[8][8];
+
+int smalltobig(int file,int rank);
+void init120to64();
+void print12064();
+int SQ64(int sq120);
+void PrintBitBoard(uint64_t bb);
+int popBitBoard(uint64_t *bb);
+int countBitBoard(uint64_t bb);
+void initsquare120();
+int fileCharToIndex(char c);
+int rankCharToIndex(char c);
+int getSquareFromString(const string& sq);
+
+int main()
+{
+    init120to64();
+    //print12064();
+    initsquare120();
+    uint64_t bb=0ULL;
+    PrintBitBoard(bb);   
+    //bb=8ULL;
+    
+    int sq120=getSquareFromString("d2");
+    bb|=(1ULL<<SQ64(sq120));
+    cout<<"COUNT : "<<countBitBoard(bb)<<endl;
+    PrintBitBoard(bb);  
+
+    sq120=getSquareFromString("d3");
+    bb|=(1ULL<<SQ64(sq120));
+    cout<<"COUNT : "<<countBitBoard(bb)<<endl;
+    PrintBitBoard(bb);
+
+    sq120=getSquareFromString("d4");
+    bb|=(1ULL<<SQ64(sq120));
+    cout<<"COUNT : "<<countBitBoard(bb)<<endl;
+    PrintBitBoard(bb);
+
+    sq120=getSquareFromString("d7");
+    bb|=(1ULL<<SQ64(sq120));
+    cout<<"COUNT : "<<countBitBoard(bb)<<endl;
+    PrintBitBoard(bb);
+
+    cout<<"POPPED : "<<popBitBoard(&bb)<<endl; PrintBitBoard(bb);
+    cout<<"POPPED : "<<popBitBoard(&bb)<<endl; PrintBitBoard(bb);
+    cout<<"POPPED : "<<popBitBoard(&bb)<<endl; PrintBitBoard(bb);
+    
+   
+    //ASSERT(5==4);
+    cout<<"end";
+}
