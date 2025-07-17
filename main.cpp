097150d602df17bce1961e37f3a589d4d94ab6ba
Nit2007 <nithishjaisarun.a@gmail.com>
2025-07-17 18:10:09 +0530

    Update main.cpp
    ADDED CORRECT s_undo and s_board elements

--- main.cpp
+++ main.cpp
@@ -26,24 +26,31 @@ struct s_undo
 {
     int move;
     int castleperm;
-    //uint64_t poskey;
+    int enpas;
+    int fifty;
+    uint64_t poskey;
 };
 
 struct s_board
 {
-    int side;
     int pieces[BOARD_SQ_NUM];
-    //uint64_t pawns[3];
+    
+    uint64_t pawns[3];
     int king[3];
-    //int bigpce[3];
+    int bigpce[3];
     int majpce[3];
     int minpce[3];
 
-
-    int castleperm;
-    int fifty;
+    int side;
     int enpas;
-    //uint64_t poskey;
+    int fifty;
+    
+    int ply;
+    int hisply;
+    //int castleperm;
+    
+    uint64_t poskey;
+
     int piecelist[13][10];
     s_undo history[MAXGAMEMOVES];//vector<s_undo>history(MAXGAMEMOVES);
 
