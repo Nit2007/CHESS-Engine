87d7a73c2d78b6ee52b03d0f8d475d3dc9d03f81
Nit2007 <nithishjaisarun.a@gmail.com>
2025-07-23 12:14:00 +0530

    Update main.cpp
    verified input and output for parseFEN and printBoard

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ int main()
      
     s_board B;
     Parse_FEN((char*)START_FEN, &B); // cast and address-of for pointer
-    PrintBoard(B);
+    PrintBoard(&B);
     Parse_FEN((char*)FEN_1, &B);
-    PrintBoard(B);
+    PrintBoard(&B);
     Parse_FEN((char*)FEN_2, &B);
-    PrintBoard(B);
-    ;Parse_FEN((char*)FEN_3, &B);
-    PrintBoard(B);
+    PrintBoard(&B);
+    Parse_FEN((char*)FEN_3, &B);
+    PrintBoard(&B);
     
-    cout<<"*************end*************"<<endl<<endl<<endl;
+    
+    cout<<endl<<"*************end*************"<<endl<<endl<<endl;
 }
