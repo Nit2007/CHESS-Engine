90d9d75b63b320a678076ac9092f4889ea78277c
Nit2007 <nithishjaisarun.a@gmail.com>
2025-07-30 05:48:01 +0530

    Update main.cpp
    removed all functions at int main() except allinit()

--- main.cpp
+++ main.cpp
@@ -6,15 +6,7 @@ int main()
 {
     allinit();
      
-    s_board B;
-    Parse_FEN((char*)START_FEN, &B); // cast and address-of for pointer
-    PrintBoard(&B);
-    Parse_FEN((char*)FEN_1, &B);
-    PrintBoard(&B);
-    Parse_FEN((char*)FEN_2, &B);
-    PrintBoard(&B);
-    Parse_FEN((char*)FEN_3, &B);
-    PrintBoard(&B);
+   
     
     
     cout<<endl<<"*************end*************"<<endl<<endl<<endl;
