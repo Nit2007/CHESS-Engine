fe18953222996b94355f40edaad915a1b90fa198
Nit2007 <nithishjaisarun.a@gmail.com>
2025-08-08 07:10:36 +0530

    Create io.cpp
    completed char *PrSq(const int sq)
    char *PrMove(const int move)

---
+++ io.cpp
@@ -0,0 +1,38 @@
+#include "defs.h"
+#include "struct.h"
+
+char *PrSq(const int sq)
+{
+    static char sqstr[3];
+    int file=FilesBrd[sq];
+    int rank=RanksBrd[sq];
+    sqstr[0]='a'+file;
+    sqstr[1]='1'+rank;//sqstr[2] = '\0';
+  return sqstr;
+}
+
+char *PrMove(const int move)
+{
+    static char mvstr[6];
+    int filefrom=FilesBrd[FROMSQ(move)];
+    int rankfrom=RanksBrd[FROMSQ(move)];
+    int fileto=FilesBrd[TOSQ(move)];
+    int rankto=RanksBrd[TOSQ(move)];
+    int promoted=PROMOTED(move);
+    if(promoted)
+    {
+      char pchar='q';
+      if(IsKn(promoted))pchar='n';
+      if(IsRQ(promoted) && !IsBQ(promoted))pchar='r';
+      if(!IsRQ(promoted) && IsBQ(promoted))pchar='b';
+       mvstr[4]=pchar;
+       mvstr[5]='\0';
+    }
+  else mvstr[4]='\0';
+    mvstr[0]='a'+filefrom;
+    mvstr[1]='1'+rankfrom;
+    mvstr[2]='a'+fileto;
+    mvstr[3]='1'+rankto;
+
+  return mvstr;
+}
