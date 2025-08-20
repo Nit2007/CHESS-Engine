788f459442bfe24d4cfae7f320941ad9bcd8f3b0
Nit2007 <nithishjaisarun.a@gmail.com>
2025-08-21 05:14:21 +0530

    Update defs.h
    added FEN for QUEEN,ROOK,BISHOP for both colour (sliding pce)

--- defs.h
+++ defs.h
@@ -16,6 +16,14 @@ using namespace std;
 #define PAWNMOVES_B_FEN "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
 #define KNIGHTKING_FEN "4k3/1n6/3n4/2N5/8/3N4/8/5K2 w - - 0 1"
 #define KNIGHTKING_B_FEN "5k2/8/3n4/8/2n5/3N4/1N6/4K3 b - - 0 1"
+#define ROOK_FEN "6k1/8/5r2/8/1nR5/5N2/8/6K1 w - - 0 1"
+#define ROOK_B_FEN "6k1/8/5n2/1Nr5/8/5R2/8/6K1 b - - 0 1"
+#define QUEEN_FEN "6k1/8/4nq2/8/1nQ5/5N2/1N6/6K1 w - - 0 1"
+#define QUEEN_B_FEN "6k1/1n6/5n2/1Nq5/8/4NQ2/8/6K1 b - - 0 1"
+#define BISHOP_FEN "6k1/1b6/4n3/8/1n4B1/1B3N2/1N6/2b3K1 b - - 0 1"
+#define BISHOP_B_FEN "2B3k1/1n6/1b3n2/1N4b1/8/4N3/1B6/6K1 w - - 0 1"
+
+
 // You define this only in debugging
 #define DEBUG       
 
