cb5fd60989d44d6debb0014c46a264999bd3404b
Nit2007 <nithishjaisarun.a@gmail.com>
2025-09-01 07:24:30 +0530

    Update search.cpp
    added assert at IsRepetition to check index bounds

--- src/search.cpp
+++ src/search.cpp
@@ -4,7 +4,7 @@
 int IsRepetition(s_board* pos)
 {
     for(int index=pos->hisply-pos->fifty;index<pos->hisply-1;index++)
-      {
+      {    ASSERT(index>=0 && index <2048);
           if(pos->poskey == pos->history[index].poskey)return TRUE;
       }
   return FALSE;
