2b0e071061e2dd97c649dc416e5dbd2b778e5bbe
Nit2007 <nithishjaisarun.a@gmail.com>
2025-09-14 18:15:58 +0530

    Update search.cpp
    basic skeleton code (preparation of Search)

--- src/search.cpp
+++ src/search.cpp
@@ -1,6 +1,10 @@
 #include "defs.h"
 #include "struct.h"
 
+static void CheckUp()
+{//to check if time is up to interupt GUI
+    
+}
 int IsRepetition(s_board* pos)
 {
     for(int index=pos->hisply-pos->fifty;index<pos->hisply-1;index++)
@@ -9,3 +13,50 @@ int IsRepetition(s_board* pos)
       }
   return FALSE;
 }
+
+static void ClearForSearch(s_board* pos, s_searchinfo* info)
+{
+    for(int i=0;i<13;i++)
+        {
+            for(int j=0;j<120;j++)
+            {
+                pos->searchHistory[i][j]=0;
+            }
+        }
+    for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<64;j++)
+            {
+                pos->searchKillers[i][j]=0;
+            }
+        }
+    ClearHashTable(pos->hashtable);
+    pos->ply=0;//Make the current ply search to zero ,not hisply which represents the total no of half moves made
+
+    pos->hashtable->overWrite=0;
+	pos->hashtable->hit=0;
+	pos->hashtable->cut=0;
+
+	info->starttime = GetTimeMs();
+	info->stoptime = 0;
+	info->nodes = 0;
+	info->fh = 0;
+	info->fhf = 0;
+}
+
+
+static int AlphaBeta(int alpha,int beta,s_board* pos, s_searchinfo* info)
+{
+    return 0;
+}
+
+static int Quiescence(int alpha,int beta,int depth ,s_board* pos, s_searchinfo* info,int DoNULL)
+{//To avoid Horizon affect of AlphaBeta (due to depth constraints)
+    return 0;
+}
+
+void SearchPosition(s_board* pos, s_searchinfo* info)
+{
+    
+}
+
